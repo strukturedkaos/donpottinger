64bc1b29c6dc7fb088a1eee3f54e9c3682a28bbd
Don Pottinger <don.pottinger@gmail.com>
2014-11-19 16:46:11 -0500

    Adds dynamic about section.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,10 @@
-//= require bootstrap
 //= require_tree .
+
+var toggleAbout = function(ev) {
+  $('.about-overlay').toggleClass('open');
+};
+
+$(document).ready(function(){
+  $(document).on('click', '.about-link', toggleAbout);
+  $(document).on('click', '.about-close', toggleAbout);
+});
