702a06d3c009135379308a928c06015739f2a032
Don Pottinger <don.pottinger@gmail.com>
2014-12-03 13:09:00 -0500

    Adds about section to screen sizes smaller than 640px.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,4 +8,5 @@ var toggleAbout = function(ev) {
 $(document).ready(function(){
   $(document).on('click', '.about-link', toggleAbout);
   $(document).on('click', '.about-close', toggleAbout);
+  $(document).on('click', '.about-mobile', toggleAbout);
 });
